adff77398de58f423cf15d756db9aab037d03888
takuma080709 <takuma593@gmail.com>
2024-09-18 19:25:54 +0900

    create sns-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/signin" element={<Signin />}/>
+          <Route path="/signup" element={<Signup />}/>
+        </Routes>
+      </div>
     </div>
+  </BrowserRouter>
   );
 }
 
---
+++ src/SessionProvider.js
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+import { AuthRepository } from "./repositories/auth";
+
+const SessionContext = createContext();
+const SessionProvider = ( props ) => {
+    const [currentUser, setCurrentUser] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        setSession();
+    }, []);
+
+    const setSession = async () => {
+        const currentUser= await AuthRepository.getCurrentUser();
+        setCurrentUser(currentUser);
+        setIsLoading(false);
+    }
+
+    if (isLoading) return <div />
+    return (
+        <SessionContext.Provider value={{ currentUser, setCurrentUser }}>
+            {props.children}
+        </SessionContext.Provider>
+    );
+}
+
+export { SessionContext, SessionProvider };
